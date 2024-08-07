c7e0138347737e3fccca206b0ac13547746ae69a
Abhishek <abhishekganesh2002@gmail.com>
2024-08-07 20:05:25 +0530

    adding files

---
+++ First Missing Positive.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        num_set = set(nums) 
+        for i in range(1, len(nums) + 1):
+            if i not in num_set:
+                return i
+        return len(nums) + 1
