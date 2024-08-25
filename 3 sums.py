b3ac1f7dce9cff715469f41b9d8045ecfffbb7c8
Abhishek <abhishekganesh2002@gmail.com>
2024-08-25 12:44:27 +0530

    Adding files

---
+++ 3 sums.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result=[]
+        if len(nums)<3:
+            return []
+        for i in range(0,len(nums)-2):
+            if nums[i]==nums[i-1] and i>0:
+                continue
+            a=nums[i]
+            l=i+1
+            r=len(nums)-1
+            while l<r:
+                s=nums[i]+nums[l]+nums[r]
+                if s==0:
+                    result.append([nums[i],nums[l],nums[r]])
+                    l+=1
+                    r-=1
+                    while nums[l]==nums[l-1] and l<r:
+                        l+=1
+                    while nums[r]==nums[r+1] and l<r:     
+                        r-=1
+                elif s<0:
+                    l+=1
+                    while nums[l]==nums[l-1] and l<r:
+                        l+=1
+                elif s>0:
+                    r-=1
+                    while nums[r]==nums[r+1] and l<r:
+                        r-=1
+        return result
--- Count Odd Numbers in an Interval Range.py
+++ Count Odd Numbers in an Interval Range.py
@@ -5,10 +5,3 @@ class Solution:
         if (low%2!=0 and high%2!=0) or (low%2==0 and high%2!=0) or (low%2!=0 and high%2==0):
             return oddCount
         return evenCount
-class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        oddCount=int(((high-low)/2)+1)
-        evenCount=int((high-low)/2)
-        if (low%2!=0 and high%2!=0) or (low%2==0 and high%2!=0) or (low%2!=0 and high%2==0):
-            return oddCount
-        return evenCount
