3395552e98aa5688c687b3065e25395fd32595a8
Abhishek G <abhishekganesh2002@gmail.com>
2024-09-15 23:22:40 +0530

    adding file

---
+++ 46 - Permutations.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, end):
+            if start == end:
+                result.append(nums[:])  
+            else:
+                for i in range(start, end):
+                    nums[start], nums[i] = nums[i], nums[start]
+                    backtrack(start + 1, end)
+                    nums[start], nums[i] = nums[i], nums[start]
+
+        result = []
+        backtrack(0, len(nums)) 
+        return result
