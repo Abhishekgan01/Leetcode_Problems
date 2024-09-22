2000eb2436c963115b74a76e8ca0fcb483e0921f
Abhishek G <abhishekganesh2002@gmail.com>
2024-09-22 22:45:28 +0530

    adding file

---
+++ 49 - Group Anagrams.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = {}
+        
+        for s in strs:
+            sorted_str = ''.join(sorted(s)) 
+            if sorted_str in anagram_map:
+                anagram_map[sorted_str].append(s)
+            else:
+                anagram_map[sorted_str] = [s] 
+
+        return list(anagram_map.values())  
+  
