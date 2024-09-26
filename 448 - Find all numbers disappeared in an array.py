983e44d27c1c3887428c5376e514574b4c34c15e
Abhishek G <abhishekganesh2002@gmail.com>
2024-09-26 19:36:11 +0530

    adding file

--- 448 - Find all numbers disappeared in an array.py
+++ 448 - Find all numbers disappeared in an array.py
@@ -6,3 +6,18 @@ class Solution:
             ind = abs(i) - 1
             nums[ind] = -abs(nums[ind])
         return [index+1 for index,number in enumerate(nums) if number>0]
+    
+#2nd method
+# class Solution:
+#     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        
+#         d = {}
+#         for i in range(1, len(nums)+1):  
+#             d[i] = 0
+#         for i in nums:
+#             d[i] += 1
+#         result = []
+#         for key, val in d.items():
+#             if val == 0:
+#                 result.append(key)
+#         return result
