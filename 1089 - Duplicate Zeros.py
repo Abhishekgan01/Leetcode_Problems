280ec6ed9476130ed4f0f9d0e6abc7b5a5a99535
Abhishek G <abhishekganesh2002@gmail.com>
2024-09-30 13:05:53 +0530

    adding file

---
+++ 1089 - Duplicate Zeros.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i=0
+        while i<len(arr)-1:
+            if arr[i]==0:
+                arr.insert(i,0)
+                arr.pop()
+                i+=1
+            i+=1
