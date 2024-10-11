61a087c53074d676f242e6a503682d0cbc9ac785
Abhishek G <abhishekganesh2002@gmail.com>
2024-10-11 22:40:29 +0530

    adding file

---
+++ 1941. Check if All Characters Have Equal Number of Occurrences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        freq = {}
+        for i in s:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i] += 1
+
+        first_freq = list(freq.values())[0]
+        
+        for count in freq.values():
+            if count != first_freq:
+                return False
+        
+        return True
