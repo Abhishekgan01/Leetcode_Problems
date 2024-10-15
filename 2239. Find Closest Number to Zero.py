26be461ede2238eb543ba9b77354f95071eb03c3
Abhishek G <abhishekganesh2002@gmail.com>
2024-10-15 16:25:25 +0530

    adding file

--- 2239. Find Closest Number to Zero.py
+++ 2239. Find Closest Number to Zero.py
@@ -1,9 +1,24 @@
-nums = [-4,-2,1,4,8]
-nums1 = []
-for i in nums:
-    nums1.append(abs(i))
-nums1.sort()
-print(nums1)
-for i in range(len(nums1)):
-    if i > 0:
-        
+from typing import List
+
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        p = []
+        n = []
+
+        for i in nums:
+            if i == 0:
+                return 0
+            elif i < 0:
+                n.append(i)
+            else:
+                p.append(i)
+
+        if not p:
+            return max(n)
+        if not n:
+            return min(p)
+
+        if abs(max(n)) < abs(min(p)):
+            return max(n)
+        else:
+            return min(p)
