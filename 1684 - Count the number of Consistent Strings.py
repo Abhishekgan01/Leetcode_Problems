f0fea13417f6c5698384e7397a4c5d56c64c895b
Abhishek G <abhishekganesh2002@gmail.com>
2024-10-29 15:46:16 +0530

    adding file

---
+++ 1684 - Count the number of Consistent Strings.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        length = len(words)
+        for i in words:
+            for j in i:
+                if j not in allowed:
+                    length-=1
+                    break
+        return length
