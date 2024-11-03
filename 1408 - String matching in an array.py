d17cf8f7b2ac89cfcd50ee8aa5cb0a3bb68be8b6
Abhishek G <abhishekganesh2002@gmail.com>
2024-11-03 18:03:35 +0530

    adding file

--- 1408 - String matching in an array.py
+++ 1408 - String matching in an array.py
@@ -1,11 +1,11 @@
 from typing import List
-
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         result = []
         for word in words:
             for i in words:
-                if word in i and word != i:
+                if word in i and word != i: #Not equal 
                     result.append(word)
                     break
+
         return result
