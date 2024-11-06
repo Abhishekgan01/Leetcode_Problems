d295da7149e37baaaebb92f7ee7f178c746c4e06
Abhishek G <abhishekganesh2002@gmail.com>
2024-11-06 23:06:07 +0530

    second commit

--- 88 - Merged Sorted Array.py
+++ 88 - Merged Sorted Array.py
@@ -12,7 +12,7 @@ class Solution:
                 n-=1
             last-=1
 
-        #filling the nums1 with leftover num2 elements
+        #filling the nums1 list with leftover num2 elements
         while n> 0:
             nums1[last] = nums2[n-1]
             n , last = n-1, last-1
