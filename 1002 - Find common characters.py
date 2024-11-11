5719040104936908d50e85781f93d21b8cfe7ff7
Abhishek G <abhishekganesh2002@gmail.com>
2024-11-11 16:16:29 +0530

    adding file

---
+++ 1002 - Find common characters.py
@@ -0,0 +1,7 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        common_count = Counter(words[0])
+        for word in words[1:]:
+            common_count &= Counter(word)
+        result = list(common_count.elements())
+        return result
