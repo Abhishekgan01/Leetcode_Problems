f9b954ea67ee1eef25732aeee8465d49bbdf6117
Abhishek G <abhishekganesh2002@gmail.com>
2024-11-19 06:12:05 +0530

    second commit

--- 300 - Longest Increasing Subsequence.py
+++ 300 - Longest Increasing Subsequence.py
@@ -5,7 +5,7 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
          sub = []
          for num in nums:
-            i = bisect.bisect_left(sub, num) #returns index
+            i = bisect.bisect_left(sub, num) #finds the index where the value should be inserted
             if i < len(sub):
                 sub[i] = num
             else:
