ba577247b0df4c2cc5864a6f44056b04e51b3dd3
Abhishek G <abhishekganesh2002@gmail.com>
2024-11-23 08:22:27 +0530

    second commit

--- 2788 - Split string by seperators.py
+++ 2788 - Split string by seperators.py
@@ -4,7 +4,7 @@ class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
         result = []
         for word in words:
-            parts = [part for part in word.split(separator) if part]
+            parts = (part for part in word.split(separator) if part)
             result.extend(parts)
         
         return result
